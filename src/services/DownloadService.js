571e9691859df9b3c08378fc4716ca198e5beaf0
Naman <namg3347@gmail.com>
2026-05-14 14:44:10 +0530

    mixed minor bug

--- src/services/DownloadService.js
+++ src/services/DownloadService.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://torqueshare.onrender.com";
 export async function fetchDownloadData(slug) {
 
     const response = await fetch(
-        `${BASE_URL}/api/content/${slug}`
+        `${BASE_URL}/contents/download${slug}`
     );
 
     await handleResponse(response);
