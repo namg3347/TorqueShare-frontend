438e49d2a6f7fd6aa8af6c56b7acf13a20b8ee62
Naman <namg3347@gmail.com>
2026-05-14 10:00:29 +0530

    changed the backend base-url from local host

--- src/services/DownloadService.js
+++ src/services/DownloadService.js
@@ -1,6 +1,6 @@
 import { handleResponse } from "./httpClient";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://torqueshare.onrender.com";
 
 export async function fetchDownloadData(slug) {
 
--- src/services/UploadService.js
+++ src/services/UploadService.js
@@ -1,6 +1,6 @@
 import { handleResponse } from "./HttpClient";
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://torqueshare.onrender.com";
 
 export async function generateUploadUrl(payload) {
 
